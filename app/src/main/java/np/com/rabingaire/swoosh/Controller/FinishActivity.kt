95420ab29a330a9596028288da6859387003b8ea
Rabin Gaire <rabingaire20@gmail.com>
2018-05-27 23:15:50 +0545

    minor changes

--- app/src/main/java/np/com/rabingaire/swoosh/Controller/FinishActivity.kt
+++ app/src/main/java/np/com/rabingaire/swoosh/Controller/FinishActivity.kt
@@ -7,17 +7,13 @@ import np.com.rabingaire.swoosh.R
 import np.com.rabingaire.swoosh.Utilities.*
 
 class FinishActivity : AppCompatActivity() {
-
-    var league = ""
-    var skill = ""
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
 
-        league = intent.getStringExtra(EXTRA_LEAGUE)
-        skill = intent.getStringExtra(EXTRA_SKILL)
+        val league = intent.getStringExtra(EXTRA_LEAGUE)
+        val skill = intent.getStringExtra(EXTRA_SKILL)
 
-        finishText.text = "Looking for a ${league} ${skill} league near you..."
+        finishText.text = "Looking for a $league $skill league near you..."
     }
 }
